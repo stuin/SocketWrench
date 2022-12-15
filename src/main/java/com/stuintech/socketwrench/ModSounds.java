faebe0fc5a9af63657aedf23db49a184ff1baa7e
stuin <stuin01@gmail.com>
2022-12-15 15:45:42 -0800

    - Reverted to 1.19.2 for fixes

--- src/main/java/com/stuintech/socketwrench/ModSounds.java
+++ src/main/java/com/stuintech/socketwrench/ModSounds.java
@@ -1,18 +1,17 @@
 package com.stuintech.socketwrench;
 
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModSounds {
     public static final Identifier wrenchSoundID = new Identifier(SocketWrench.MODID, "default_wrench");
     public static final Identifier modeSoundID = new Identifier(SocketWrench.MODID, "default_mode_change");
-    public static final SoundEvent wrenchSound = SoundEvent.of(wrenchSoundID);
-    public static final SoundEvent modeSound = SoundEvent.of(modeSoundID);
+    public static final SoundEvent wrenchSound = new SoundEvent(wrenchSoundID);
+    public static final SoundEvent modeSound = new SoundEvent(modeSoundID);
 
     public static void register() {
-        Registry.register(Registries.SOUND_EVENT, wrenchSoundID, wrenchSound);
-        Registry.register(Registries.SOUND_EVENT, modeSoundID, modeSound);
+        Registry.register(Registry.SOUND_EVENT, wrenchSoundID, wrenchSound);
+        Registry.register(Registry.SOUND_EVENT, modeSoundID, modeSound);
     }
 }
